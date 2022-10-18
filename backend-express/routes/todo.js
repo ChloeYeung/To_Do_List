2b4b3f94c790d67dc4fb6ee73cf18be13fcb6128
jackykwandesign <jackykwandesign@gmail.com>
2022-10-18 19:02:02 +0000

    implement restful curd of todo

--- backend-express/routes/todo.js
+++ backend-express/routes/todo.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const isDefined = require('../utils/isDefined')
 const responseHandler = require("../utils/responseFormat")
-const { getTodoList, addTodoItem } = require("../service/todo.service")
+const { getTodoList, addTodoItem, updateTodoItemById, deleteTodoItemById } = require("../service/todo.service")
 /* GET users listing. */
 router.get('/', async function (req, res) {
   try {
@@ -20,10 +20,35 @@ router.post('/', async function (req, res) {
   }
   try {
     const newTodoItem = await addTodoItem(req.user.id, item)
-    return responseHandler({ res, code: 200, data:newTodoItem })
+    return responseHandler({ res, code: 201, data:newTodoItem })
   } catch (error) {
     return responseHandler({ res, errorMessage: error.message })
   }
 });
+
+router.patch('/:id', async function (req, res) {
+  const {id} = req.params
+  const {edit} = req.body 
+  if (!isDefined(edit)) {
+    return responseHandler({ res, code: 400 })
+  }
+  try {
+    const editdTodoItem = await updateTodoItemById(id, edit)
+    return responseHandler({ res, code: 200, data:editdTodoItem })
+  } catch (error) {
+    return responseHandler({ res, errorMessage: error.message })
+  }
+});
+
+router.delete('/:id', async function (req, res) {
+  const {id} = req.params
+  try {
+    const deleted = await deleteTodoItemById(id)
+    return responseHandler({ res, code: 200 })
+  } catch (error) {
+    return responseHandler({ res, errorMessage: error.message })
+  }
+});
+
 
 module.exports = router;
